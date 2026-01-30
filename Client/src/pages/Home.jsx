49c3c8e81638a59acc2337132b6bd06e5f0451d0
Anshitva7Mishra <mishraanshitva@gmail.com>
2026-01-30 11:55:34 +0530

    HelloAndHii3445

---
+++ Client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
